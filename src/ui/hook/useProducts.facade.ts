7a378ef69f8253c1a5c7cb7197e6e866444cd311
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2025-01-18 14:44:44 +0100

    wip: create useProducts.facade.ts with interface Product

---
+++ src/ui/hook/useProducts.facade.ts
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
+export const useProducts = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+};
