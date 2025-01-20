85f6af54df1955ebe54a004f5cd0d0921264b504
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2025-01-20 16:36:40 +0100

    feat: add SafeAreaView on all the app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 // In App.js in a new project
 
+import React from 'react';
 import RootStack from './src/ui/navigation/root.stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
