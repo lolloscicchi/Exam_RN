40d303048ad10de9877724554e78c66d78784fec
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2025-01-20 20:50:36 +0100

    feat: add dedail on types.ts

--- src/ui/navigation/types.ts
+++ src/ui/navigation/types.ts
@@ -7,9 +7,14 @@ export enum Screen {
 export type TabParams = {
   [Screen.Home]: undefined; // Home ha il proprio Stack interno
   [Screen.Favorite]: undefined;
+  [Screen.Detail]: undefined;
 };
 
 // Per il RootStack
 export type MainParamList = {
   TabNavigator: undefined;
+  [Screen.Detail]: {
+    id: number;
+    ids: number[];
+  };
 };
