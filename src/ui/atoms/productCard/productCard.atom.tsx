5d82880b6bcc8b3c8c71f740387df34ed8b554ee
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2025-01-20 17:02:18 +0100

    feat: add the style to the productCard

--- src/ui/atoms/productCard/productCard.atom.tsx
+++ src/ui/atoms/productCard/productCard.atom.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { styles } from './productCard.styles';
-import { Product } from '../../hook/useProducts.facade';
+import { Product } from '../../models/product.model';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const ProductCard = ({ product }: Product) => {
   return (
     <View style={styles.card}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <View style={styles.imageButtonContainer}>
+        <Image source={{ uri: product.image }} style={styles.image} />
 
-      <Text style={styles.title}>{product.title}</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity>
+            <Ionicons name={'bookmark'} size={40} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name={'bookmark-outline'} size={40} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{product.title}</Text>
 
-      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+        <Text style={styles.description} numberOfLines={2}>
+          {product.description}
+        </Text>
+        <Text style={styles.price}>${product.price.toFixed(2)}</Text>
 
-      <Text style={styles.description}>{product.description}</Text>
-
-      <Text style={styles.rating}>
-        Rating: {product.rating.rate} ({product.rating.count} reviews)
-      </Text>
+        <Text style={styles.rating}>
+          Rating: {product.rating.rate} ({product.rating.count} reviews)
+        </Text>
+      </View>
     </View>
   );
 };
--- src/ui/atoms/productCard/productCard.styles.ts
+++ src/ui/atoms/productCard/productCard.styles.ts
@@ -2,7 +2,9 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   card: {
-    backgroundColor: '#fff',
+    backgroundColor: '#f3f6f0',
+    flexDirection: 'row',
+    flex: 1,
     borderRadius: 8,
     padding: 16,
     margin: 8,
@@ -12,12 +14,24 @@ export const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 3,
   },
-  image: {
-    width: '100%',
-    height: 200,
-    resizeMode: 'contain',
-    marginBottom: 16,
+  imageButtonContainer: {
+    flex: 2,
+    justifyContent: 'space-between',
   },
+  image: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 10,
+    width: 128,
+    resizeMode: 'contain',
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  textContainer: { flex: 3, resizeMode: 'contain' },
   title: {
     fontSize: 18,
     fontWeight: 'bold',
