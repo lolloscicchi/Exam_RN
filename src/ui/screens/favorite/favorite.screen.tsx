b3cfa77b5e62ead2ccc33983884417a9404dddad
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2025-01-20 20:45:36 +0100

    feat: favourite.screen show favourite products

--- src/ui/screens/favorite/favorite.screen.tsx
+++ src/ui/screens/favorite/favorite.screen.tsx
@@ -1,11 +1,73 @@
-import { Button, Text, View } from 'react-native';
+import React, { useCallback, useEffect } from 'react';
+import { FlatList, ListRenderItem, View } from 'react-native';
 import { memo } from 'react';
+import { useProducts } from '../../hook/useProducts.facade';
+import ProductCard from '../../atoms/productCard/productCard.atom';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { MainParamList, Screen } from '../../navigation/types';
+import { Product } from '../../models/product.model';
+import { styles } from '../home/home.styles';
 
-const FavoriteScreen = () => {
+interface Props {
+  navigation: NativeStackNavigationProp<MainParamList, Screen.Favorite>;
+}
+
+const FavoriteScreen = ({ navigation }: Props) => {
+  const {
+    products,
+    setProducts,
+    favoriteIds,
+    setFavoriteIds,
+    refreshProducts,
+    loadFavorites,
+    updateFavoriteIds,
+  } = useProducts();
+
+  // ** CALLBACKS ** //
+
+  const renderItem = useCallback<ListRenderItem<Product>>(
+    ({ item }) => {
+      return (
+        <ProductCard
+          product={item}
+          openCart={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+          isFavorite={favoriteIds.includes(item.id)}
+          switchFavorite={() => {
+            updateFavoriteIds(item);
+          }}
+        />
+      );
+    },
+    [favoriteIds, updateFavoriteIds]
+  );
+
+  const ItemSeparatorComponent = useCallback(
+    () => <View style={styles.itemSeparatorComponent}></View>,
+    []
+  );
+  // ** USE EFFECTS ** //
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      refreshProducts();
+      loadFavorites();
+      console.log(navigation);
+    });
+    return unsubscribe;
+  }, [favoriteIds, loadFavorites, navigation, refreshProducts]);
+
+  // ** UI ** //
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>{'FAVORITE SCREEN'}</Text>
-      <Button title={'Go to Home'} />
+    <View style={styles.container}>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        ItemSeparatorComponent={ItemSeparatorComponent}
+        data={products.filter((product) => favoriteIds.includes(product.id))}
+        renderItem={renderItem}
+      />
     </View>
   );
 };
---
+++ src/ui/screens/favorite/favorite.styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#c0ddd1',
+  },
+  itemSeparatorComponent: { height: 20 },
+});
