c43014766a804c2caa0af786a9671b68f87b2123
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2025-01-23 17:19:20 +0100

    feat: add primary color to status bar and refactor styles

--- App.styles.ts
+++ App.styles.ts
@@ -1,3 +1,4 @@
 import { StyleSheet } from 'react-native';
+import { COLORS } from './src/ui/theme/colors.theme';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({ safeArea: { flex: 1, backgroundColor: COLORS.primary } });
--- App.tsx
+++ App.tsx
@@ -3,11 +3,16 @@
 import React from 'react';
 import RootStack from './src/ui/navigation/root.stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
+import { COLORS } from './src/ui/theme/colors.theme';
+import { styles } from './App.styles';
 
 export default function App() {
+  StatusBar.setBackgroundColor(COLORS.primary);
+  StatusBar.setBarStyle('light-content');
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={styles.safeArea}>
       <NavigationContainer>
         <RootStack />
       </NavigationContainer>
